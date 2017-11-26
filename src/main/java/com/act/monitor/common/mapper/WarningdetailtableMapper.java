77bf7aa2aaae0a41ae5cd9fc91b12d34322e69d4
movescar@163.com <huhuan66612>
2017-11-26 22:41:20 +0800

    第二次提交

---
+++ src/main/java/com/act/monitor/common/mapper/WarningdetailtableMapper.java
@@ -0,0 +1,7 @@
+package com.act.monitor.common.mapper;
+
+import com.act.monitor.common.model.Warningdetailtable;
+import com.act.monitor.common.utils.MyMapper;
+
+public interface WarningdetailtableMapper extends MyMapper<Warningdetailtable> {
+}
